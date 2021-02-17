7d418bdc68b34228a788fe9dd69f20a347891a18
강덕현 <9changjo@gmail.com>
2021-02-17 15:54:20 +0900

    add routers
    routers : user, video, global

--- media/app.js
+++ media/app.js
@@ -3,11 +3,14 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import {userRouter} from "./router.js";
+import userRouter from "./routers/userRouter.js";
+import videoRouter from "./routers/videoRouter.js";
+import globalRouter from "./routers/globalRouter.js";
+
 const app = express();
-const handleListening = (req, res) => console.log(`listening on http://localhost:${PORT}`);
+/*const handleListening = (req, res) => console.log(`listening on http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send("Hello from home");
-const handleProfile = (req, res) => res.send("You are on my profile");
+const handleProfile = (req, res) => res.send("You are on my profile");*/
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -15,8 +18,10 @@ app.use(bodyParser.urlencoded({extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
+/*app.get("/", handleHome);
+app.get("/profile", handleProfile);*/
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
